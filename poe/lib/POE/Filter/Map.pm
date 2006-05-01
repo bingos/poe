bingos/poe

commit efbf238fdda80be3ff8103c93464eb64c7a08808

Patched Filter::Map to close rt18551 Added sanity check to modify()
git-svn-id: https://poe.svn.sourceforge.net/svnroot/poe/trunk@1948 742d25e2-4c0f-0410-ba8b-f6b7b6679366

poe/lib/POE/Filter/Map.pm
@@ -34,6 +34,11 @@ sub new {
     defined($params{Code}) or
     (defined($params{Get}) and defined($params{Put}))
   );
+  croak "Code element is not a subref"
+    unless (defined $params{Code} and ref $params{Code} eq 'CODE');
+  croak "Get or Put element is not a subref"
+    unless ((defined $params{Get} and ref $params{Get} eq 'CODE')
+    and (defined $params{Put} and ref $params{Put} eq 'CODE'));
 
   my $self = bless [
     [ ],           # BUFFER
@@ -89,6 +94,7 @@ sub modify {
   my ($self, %params) = @_;
   for (keys %params) {
     next unless ($_ eq 'Put') || ($_ eq 'Get') || ($_ eq 'Code');
+    croak "$_ element must be a subref" unless ref $params{$_} eq 'CODE';
     $self->[
       {
         Put  => CODEPUT,

commit a4c4384746eae62db0d87c866186f02a6015cfae

Edited mistake in POE::Filter::Map introduced in r1948 -This line, and those below, will be ignored--
M    lib/POE/Filter/Map.pm


git-svn-id: https://poe.svn.sourceforge.net/svnroot/poe/trunk@1952 742d25e2-4c0f-0410-ba8b-f6b7b6679366

poe/lib/POE/Filter/Map.pm
@@ -35,10 +35,10 @@ sub new {
     (defined($params{Get}) and defined($params{Put}))
   );
   croak "Code element is not a subref"
-    unless (defined $params{Code} and ref $params{Code} eq 'CODE');
+    unless (defined $params{Code} ? ref $params{Code} eq 'CODE' : 1);
   croak "Get or Put element is not a subref"
-    unless ((defined $params{Get} and ref $params{Get} eq 'CODE')
-    and (defined $params{Put} and ref $params{Put} eq 'CODE'));
+    unless ((defined $params{Get} ? (ref $params{Get} eq 'CODE') : 1)
+      and   (defined $params{Put} ? (ref $params{Put} eq 'CODE') : 1));
 
   my $self = bless [
     [ ],           # BUFFER