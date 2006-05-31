d25c7c97f9d651c868da90c8cd0302381c62fca8
rcaputo <rcaputo@742d25e2-4c0f-0410-ba8b-f6b7b6679366>
2006-05-31 15:21:38 +0000

    Reflow commit messages for IRC.
    git-svn-id: https://poe.svn.sourceforge.net/svnroot/poe/trunk@1975 742d25e2-4c0f-0410-ba8b-f6b7b6679366

--- extras/commitrelay.pl
+++ extras/commitrelay.pl
@@ -14,7 +14,7 @@ our $OUT_CHANNEL = "#poe";
 
 POE::Session->create(
   package_states => [
-    'My::Receiver' => [ qw(_start irc_001 irc_public shutdown) ],
+    'My::Receiver' => [ qw(_start irc_001 irc_public shutdown flush_commit) ],
   ],
 );
 
@@ -27,6 +27,11 @@ POE::Session->create(
 {
   package My::Receiver;
   use POE;
+
+  my %interesting_projects = (
+    poe => 1,
+  );
+
   sub _start {
     $_[KERNEL]->alias_set('receiver');
     my $irc = $_[HEAP]->{irc} = POE::Component::IRC->spawn(
@@ -43,18 +48,51 @@ POE::Session->create(
     $_[KERNEL]->post($_[SENDER] => join => $IN_CHANNEL);
   }
   sub irc_public {
-    my ($irc, $who, $where, $what) = ($_[HEAP]->{irc}, @_[ARG0..ARG2]);
+    my ($kernel, $heap) = @_[KERNEL, HEAP];
+    my ($irc, $who, $where, $what) = ($heap->{irc}, @_[ARG0..ARG2]);
     my $channel = $where->[0];
     return unless $channel eq $IN_CHANNEL;
     return unless $who =~ m/^CIA-\d+!.=cia@/i;
     $what =~ s/\cC\d+(,\d+)*//g;
     $what =~ tr/\x00-\x1f//d;
-    my ($project) = $what =~ m/(\w{3,}):/;
+    my ($project) = $what =~ m/(\w+):/;
     print "<$channel> <$who> <$project> <$what>\n";
-    return unless $project and $project eq 'poe';
-    $_[KERNEL]->post(transmitter => a_commit => $what);
-    print "  signaled transmitter\n";
+    return unless $project and exists($interesting_projects{$project});
+
+    # Remove the project.
+    $what =~ s/\w+:\s*//;
+
+    # Find the start of a commit.
+    my ($committer, $revision) = $what =~ m/(\S+)\s*\*\s*(r\d+)/;
+    if ($revision) {
+
+      # If there's a previous commit, flush it.
+      if ($heap->{commit}{$project}) {
+        $kernel->alarm_remove($heap->{delay}{$project});
+        $kernel->post(transmitter => a_commit => $project, join(" ", $heap->{commit}));
+        print "  signaled transmitter (previous commit)\n";
+      }
+
+      # Start the new commit.
+      $heap->{commit}{$project} = [ $what ];
+      $heap->{delay}{$project} = $kernel->delay_set(flush_commit => 5, $project);
+      return;
+    }
+
+    # Don't push new lines onto projects that haven't started.
+    return unless exists $heap->{commit}{$project};
+
+    push @{$heap->{commit}{$project}}, $what;
   }
+
+  sub flush_commit {
+    my ($kernel, $heap, $project) = @_[KERNEL, HEAP, ARG0];
+    $kernel->post(transmitter => a_commit => $project, join(" ", @{$heap->{commit}{$project}}));
+    delete $heap->{commit}{$project};
+    delete $heap->{delay}{$project};
+    print "  signaled transmitter (flushed $project)\n";
+  }
+
   sub shutdown {
     $_[HEAP]->{irc}->yield('shutdown');
   }
@@ -63,6 +101,11 @@ POE::Session->create(
 {
   package My::Transmitter;
   use POE;
+
+  use Text::Wrap qw(wrap $columns $huge);
+  $columns = 475;
+  $huge    = "wrap";
+
   sub _start {
     $_[KERNEL]->alias_set('transmitter');
     my $irc = $_[HEAP]->{irc} = POE::Component::IRC->spawn(
@@ -80,8 +123,16 @@ POE::Session->create(
   } 
   sub irc_public { }
   sub a_commit {
-    print "a_commit <$_[ARG0]>\n";
-    $_[HEAP]->{irc}->yield(privmsg => $OUT_CHANNEL => $_[ARG0]);
+    my ($project, $message) = @_[ARG0, ARG1];
+    my @commit = wrap("","",$message);
+
+    print "got commit: $project: $message\n";
+
+    foreach (@commit) {
+      chomp;
+      print "a_commit <$project: $_>\n";
+      $_[HEAP]->{irc}->yield(privmsg => $OUT_CHANNEL => "($project) $_");
+    }
   }
 }
 
