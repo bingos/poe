747d09c381cc2e1adade4ca2b7b7a5d9bc58dfd7
rcaputo <rcaputo@742d25e2-4c0f-0410-ba8b-f6b7b6679366>
2009-02-22 19:25:46 +0000

    Time for a new release.
    git-svn-id: https://poe.svn.sourceforge.net/svnroot/poe/trunk@2458 742d25e2-4c0f-0410-ba8b-f6b7b6679366

--- poe-test-loops/lib/POE/Test/Loops.pm
+++ poe-test-loops/lib/POE/Test/Loops.pm
@@ -6,7 +6,7 @@ use strict;
 use vars qw($VERSION);
 
 use vars qw($VERSION $REVISION);
-$VERSION = '1.003'; # NOTE - Should be #.### (three decimal places)
+$VERSION = '1.004'; # NOTE - Should be #.### (three decimal places)
 $REVISION = do {my($r)=(q$Revision$=~/(\d+)/);sprintf"0.%04d",$r};
 
 use File::Spec;
