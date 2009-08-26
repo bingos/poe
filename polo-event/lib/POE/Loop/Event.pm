2128e1eb440059dfe2bfe2ae3d299c66252bb1ed
rcaputo <rcaputo@742d25e2-4c0f-0410-ba8b-f6b7b6679366>
2009-08-26 15:31:23 +0000

    Add POE::Test::Loops to configure_requires and build_requires.
    git-svn-id: https://poe.svn.sourceforge.net/svnroot/poe/trunk@2644 742d25e2-4c0f-0410-ba8b-f6b7b6679366

--- polo-event/lib/POE/Loop/Event.pm
+++ polo-event/lib/POE/Loop/Event.pm
@@ -10,7 +10,7 @@ use strict;
 use POE::Loop::PerlSignals;
 
 use vars qw($VERSION);
-$VERSION = '1.300'; # NOTE - Should be #.### (three decimal places)
+$VERSION = '1.301'; # NOTE - Should be #.### (three decimal places)
 
 =for poe_tests
 
