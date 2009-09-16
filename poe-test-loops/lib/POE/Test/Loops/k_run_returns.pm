3b12a8a1a187c184a971cf26f5b38a45da2d5fcd
rcaputo <rcaputo@742d25e2-4c0f-0410-ba8b-f6b7b6679366>
2009-09-16 15:11:30 +0000

    Test that POE::Kernel->run() without sessions returns right away.
    git-svn-id: https://poe.svn.sourceforge.net/svnroot/poe/trunk@2691 742d25e2-4c0f-0410-ba8b-f6b7b6679366

---
+++ poe-test-loops/lib/POE/Test/Loops/k_run_returns.pm
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+# vim: ts=2 sw=2 expandtab
+
+# POE::Kernel->run() should return right away if there are no
+# sessions.
+
+use strict;
+
+use lib qw(./mylib ../mylib);
+
+sub POE::Kernel::ASSERT_DEFAULT () { 1 }
+sub POE::Kernel::TRACE_DEFAULT  () { 1 }
+sub POE::Kernel::TRACE_FILENAME () { "./test-output.err" }
+
+use POE;
+use Test::More tests => 1;
+
+{
+  my $death_note = "never returned\n";
+  local $SIG{ALRM} = sub { die $death_note };
+  alarm(10);
+  eval { POE::Kernel->run() };
+  alarm(0);
+
+  is($@, "", "POE::Kernel->run() returned right away");
+}
+
+1;
