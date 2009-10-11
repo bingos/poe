28bda629a0f41f27af9bd39720809932916b0956
rcaputo <rcaputo@742d25e2-4c0f-0410-ba8b-f6b7b6679366>
2009-10-11 18:53:43 +0000

    Fix Marco's name. Oops!
    git-svn-id: https://poe.svn.sourceforge.net/svnroot/poe/trunk@2741 742d25e2-4c0f-0410-ba8b-f6b7b6679366

--- poe-test-loops/lib/POE/Test/DondeEstan.pm
+++ poe-test-loops/lib/POE/Test/DondeEstan.pm
@@ -7,8 +7,8 @@ use strict;
 
 use File::Spec;
 
-# It's a pun on Marco Polo, the swimming game, and Macro Antonio
-# Manzo, this cool dude I know.  Hi, Marco!
+# It's a pun on Marco Polo, the swimming game, and Marco A. Manzo,
+# this cool dude I know.  Hi, Marco!
 
 sub marco {
 	my @aqui = File::Spec->splitdir(__FILE__);
