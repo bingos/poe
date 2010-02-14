790ff7c70e44fbb1e230aa50b0956a43d3fb473c
rcaputo <rcaputo@742d25e2-4c0f-0410-ba8b-f6b7b6679366>
2010-02-14 23:55:56 +0000

    Skip pty tests if IO::Pty isn't available.  Also some style fixes.
    git-svn-id: https://poe.svn.sourceforge.net/svnroot/poe/trunk@2810 742d25e2-4c0f-0410-ba8b-f6b7b6679366

--- poe-test-loops/lib/POE/Test/Loops/z_leolo_wheel_run.pm
+++ poe-test-loops/lib/POE/Test/Loops/z_leolo_wheel_run.pm
@@ -19,11 +19,14 @@ use POE::Wheel::Run;
 
 SKIP: {
   skip("The underlying event loop has trouble with ptys on $^O", 6)
-    if $^O eq "darwin" and (
-      exists $INC{"POE/Loop/IO_Poll.pm"} or
-      exists $INC{"POE/Loop/Event.pm"} or
-      $ENV{POE_LOOP_USES_POLL}
-    ) and not $ENV{POE_DANTIC};
+  if $^O eq "darwin" and (
+    exists $INC{"POE/Loop/IO_Poll.pm"} or
+    exists $INC{"POE/Loop/Event.pm"} or
+    $ENV{POE_LOOP_USES_POLL}
+  ) and not $ENV{POE_DANTIC};
+
+  eval "use IO::Pty";
+  skip("IO::Pty is not available; skipping pty tests", 6) if $@;
 
   PoeTestWorker->spawn( 'pty' );
   PoeTestWorker->spawn( 'pty-pipe' );
@@ -52,85 +55,86 @@ sub DEBUG () { 0 }
 
 sub seen
 {
-    my( $heap, $what ) = @_;
-    $heap->{seen}{$what}++;
-    DEBUG and diag "$$: seen $what\n";
-    if( 3==keys %{$heap->{seen}} ) {
-        delete $heap->{wheel};
-        $poe_kernel->alarm_remove( delete $heap->{tid} ) if $heap->{tid};
-    }
+  my( $heap, $what ) = @_;
+  $heap->{seen}{$what}++;
+  DEBUG and diag "$$: seen $what\n";
+  if( 3==keys %{$heap->{seen}} ) {
+    delete $heap->{wheel};
+    $poe_kernel->alarm_remove( delete $heap->{tid} ) if $heap->{tid};
+  }
 }
 
 
 sub spawn 
 {
-    my( $package, $conduit ) = @_;
-    POE::Session->create(
-        args => [ $conduit ],
-        inline_states => {
-            _start => sub {
-                my( $heap, $kernel, $conduit ) = @_[ HEAP, KERNEL, ARG0 ];
-                $heap->{conduit} = $conduit;
-                $heap->{seen} = {};
-                $kernel->sig_child( TERM => 'TERM' );
+  my( $package, $conduit ) = @_;
+  POE::Session->create(
+    args => [ $conduit ],
+    inline_states => {
+      _start => sub {
+        my( $heap, $kernel, $conduit ) = @_[ HEAP, KERNEL, ARG0 ];
+        $heap->{conduit} = $conduit;
+        $heap->{seen} = {};
+        $kernel->sig_child( TERM => 'TERM' );
 
-                $heap->{wheel} = POE::Wheel::Run->new(
-                        StderrEvent => ( $conduit eq 'pty' ? undef() 
-                                                           : 'stderr' ),
-                        StdoutEvent => 'stdout',
-#                        ErrorEvent  => 'error',
-                        CloseEvent  => 'closeE',
-                        Conduit     => $conduit,
-                        Program     => sub {
-                                print "hello\n";
-                                sleep 1;
-                                print "hello world" x 1024, "\n";
-                                print "done\n";
-                            }
-                    );
-                $kernel->sig_child( $heap->{wheel}->PID, 'CHLD' );
-                $heap->{tid} = $kernel->delay_set( timeout => 600 );
-            },
-            _stop => sub {
-                my( $heap, $kernel ) = @_[ HEAP, KERNEL ];
-                ## This is the money shot
-                foreach my $need ( qw( done close CHLD ) ) {
-                    is( $heap->{seen}{$need}, 1, "$heap->{conduit}: $need" );
-                }
-            },
-            timeout => sub {
-                my( $heap, $kernel ) = @_[ HEAP, KERNEL ];
-                $poe_kernel->alarm_remove( delete $heap->{tid} ) if $heap->{tid};
-                delete $heap->{wheel};
-                delete $heap->{tid};
-            },
-            TERM => sub {
-                my( $heap, $kernel ) = @_[ HEAP, KERNEL ];
-                $poe_kernel->alarm_remove( delete $heap->{tid} ) if $heap->{tid};
-                delete $heap->{wheel};
-                $kernel->sig_handled;
-            },
-            closeE => sub {
-                my( $heap, $kernel ) = @_[ HEAP, KERNEL ];
-                seen( $heap, 'close' );
-            },
-            CHLD => sub {
-                my( $heap, $kernel ) = @_[ HEAP, KERNEL ];
-                seen( $heap, 'CHLD' );
-                $kernel->sig_handled;
-            },
-            stdout => sub {
-                my( $heap, $kernel, $line, $wid ) = @_[ HEAP, KERNEL, ARG0..$#_ ];
-                seen( $heap, 'done' ) if $line eq 'done';
-            },
-            stderr => sub {
-                my( $heap, $kernel, $line, $wid ) = @_[ HEAP, KERNEL, ARG0..$#_ ];
-                warn "ERROR [$$]: $line\n";
-                seen( $heap, 'error' );
-                $poe_kernel->alarm_remove( delete $heap->{tid} ) if $heap->{tid};
-            },
+        $heap->{wheel} = POE::Wheel::Run->new(
+          StderrEvent => (
+            $conduit eq 'pty' ? undef() : 'stderr'
+          ),
+          StdoutEvent => 'stdout',
+          #ErrorEvent  => 'error',
+          CloseEvent  => 'closeE',
+          Conduit     => $conduit,
+          Program     => sub {
+            print "hello\n";
+            sleep 1;
+            print "hello world" x 1024, "\n";
+            print "done\n";
+          }
+        );
+        $kernel->sig_child( $heap->{wheel}->PID, 'CHLD' );
+        $heap->{tid} = $kernel->delay_set( timeout => 600 );
+      },
+      _stop => sub {
+        my( $heap, $kernel ) = @_[ HEAP, KERNEL ];
+        ## This is the money shot
+        foreach my $need ( qw( done close CHLD ) ) {
+          is( $heap->{seen}{$need}, 1, "$heap->{conduit}: $need" );
         }
-    );
+      },
+      timeout => sub {
+        my( $heap, $kernel ) = @_[ HEAP, KERNEL ];
+        $poe_kernel->alarm_remove( delete $heap->{tid} ) if $heap->{tid};
+        delete $heap->{wheel};
+        delete $heap->{tid};
+      },
+      TERM => sub {
+        my( $heap, $kernel ) = @_[ HEAP, KERNEL ];
+        $poe_kernel->alarm_remove( delete $heap->{tid} ) if $heap->{tid};
+        delete $heap->{wheel};
+        $kernel->sig_handled;
+      },
+      closeE => sub {
+        my( $heap, $kernel ) = @_[ HEAP, KERNEL ];
+        seen( $heap, 'close' );
+      },
+      CHLD => sub {
+        my( $heap, $kernel ) = @_[ HEAP, KERNEL ];
+        seen( $heap, 'CHLD' );
+        $kernel->sig_handled;
+      },
+      stdout => sub {
+        my( $heap, $kernel, $line, $wid ) = @_[ HEAP, KERNEL, ARG0..$#_ ];
+        seen( $heap, 'done' ) if $line eq 'done';
+      },
+      stderr => sub {
+        my( $heap, $kernel, $line, $wid ) = @_[ HEAP, KERNEL, ARG0..$#_ ];
+        warn "ERROR [$$]: $line\n";
+        seen( $heap, 'error' );
+        $poe_kernel->alarm_remove( delete $heap->{tid} ) if $heap->{tid};
+      },
+    }
+  );
 }
 
 1;
