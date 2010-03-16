4be9525d0d18c8cac21375bbe5036a81f69ea66a
apocal <apocal@742d25e2-4c0f-0410-ba8b-f6b7b6679366>
2010-03-16 07:49:11 +0000

    Delete the cached gravatars - get_gravatars.pl now works! Tweaked Gource options for a better video
    git-svn-id: https://poe.svn.sourceforge.net/svnroot/poe/trunk@2842 742d25e2-4c0f-0410-ba8b-f6b7b6679366

--- extras/gource/get_gravatars.pl
+++ extras/gource/get_gravatars.pl
@@ -1,53 +1,102 @@
 #!/usr/bin/perl
-#fetch Gravatars
 # Hacked version of the example on http://code.google.com/p/gource/wiki/GravatarExample
-# Just run it and take the usernames and convert them to PAUSE ids
-# And manually retrieve it from gravatar
+# Make sure the config is correct before running this!
+use strict; use warnings;
 
-use strict;
-use warnings;
+# Size, in px of the image
+my $size = 80;
 
-use LWP::Simple;
-use Digest::MD5 qw(md5_hex);
+# Set the path to the POE git repo
+my $gitpath = File::Spec->catdir( $ENV{HOME}, 'othergit', 'poe' );
 
-my $size       = 90;
-my $output_dir = '.git/avatar';
+# store the mapping between committer => PAUSE id for gravatar lookup
+# http://poe.dyndns.org/~troc/notes/authors.txt # for the info :)
+# This is up-to-date as of March 16, 2010
+my %author_to_pauseid = (
+	'dhoss'		=> 'DHOSS',
+	'immute'	=> 'IMMUTE',
+	'bingosnet'	=> 'BINGOS',
+	'teknikill'	=> 'XANTUS',
+	'nothingmuch'	=> 'NUFFIN',
+	'sungo'		=> 'SUNGO',
+	'cfedde'	=> 'CFEDDE',
+	'lotr'		=> 'MARTIJN',
+	'root'		=> 'RCAPUTO',
+	'tarrantsun'	=> undef,	# http://twitter.com/dscouch - I should add code to extract the profile pic from there, blah...
+	'hachi'		=> 'HACHI',
+	'bsmith'	=> 'BSMITH',
+	'rcaputo'	=> 'RCAPUTO',
+	'gwyn17'	=> 'GWYN',
+	'troc'		=> 'RCAPUTO',
+	'apocal'	=> 'APOCAL',
+	'adamkennedy'	=> 'ADAMK',
+	'(no author)'	=> 'RCAPUTO',
+	'cwest'		=> 'CTWETEN',
+);
 
-system("no .git/ directory found in current path\n") unless -d '.git';
+# END OF CONFIG
 
-mkdir($output_dir) unless -d $output_dir;
+use LWP::Simple qw( getstore get );
+use File::Spec;
 
-open(GITLOG, q/git log --pretty=format:"%ae|%an" |/) or die("failed to read git-log: $!\n");
+my $output_dir = File::Spec->catdir( $gitpath, 'extras', 'gource', 'avatars' );
 
-my %processed_authors;
-
-while(<GITLOG>) {
-    chomp;
-    my($email, $author) = split(/\|/, $_);
-
-    next if $processed_authors{$author}++;
-
-    my $author_image_file = $output_dir . '/' . $author . '.png';
-
-    #skip images we have
-    next if -e $author_image_file;
-
-    #try and fetch image
-
-    my $grav_url = "http://www.gravatar.com/avatar.php?gravatar_id=".md5_hex(lc $email)."&d=404&size=".$size; 
-
-#    warn "fetching image for '$author' $email ($grav_url)...\n";
-
-#    my $rc = getstore($grav_url, $author_image_file);
-
-#    sleep(1);
-
-#    if($rc != 200) {
-#        unlink($author_image_file);
-#        next;
-#    }
+# First of all, we move to the git path
+chdir( $gitpath ) or die "Unable to chdir to '$gitpath': $!\n";
+if ( ! -d $output_dir ) {
+	mkdir( $output_dir ) or die "Unable to mkdir '$output_dir': $!\n";
 }
 
-close GITLOG;
-use Data::Dumper;
-print Dumper( \%processed_authors );
+# Process the git log!
+open( my $gitlog, q/git log --pretty=format:"%an" |/ ) or die "Unable to read git-log: $!\n";
+my %authors;
+
+while( <$gitlog> ) {
+	chomp;
+	my $author = $_;
+	next if $authors{$author}++;
+
+	# skip images we have
+	my $author_image_file = File::Spec->catfile( $output_dir, $author . '.png' );
+	next if -e $author_image_file;
+
+	# Look up the email address of the PAUSE id, if any?
+	my $gravatar;
+	if ( exists $author_to_pauseid{ $author } ) {
+		# Some authors don't have a PAUSE id...
+		next if ! defined $author_to_pauseid{ $author };
+
+		# get it!
+		# I hate this crap code but URI::Find::Rule seems to bomb out trying to find the gravatar URL...
+		my $content = get( 'http://search.cpan.org/~' . $author_to_pauseid{ $author } );
+		if ( defined $content and $content =~ m|\<img\s+src\=\"http\://www\.gravatar\.com/avatar\.php\?gravatar_id\=([^\&]+)\&| ) {
+			$gravatar = $1;
+		} else {
+			warn "Unable to parse search.cpan.org for gravatar URL for '$author' PAUSEid: $author_to_pauseid{ $author }\n";
+			next;
+		}
+	} else {
+		warn "New committer to POE detected! Please update the mapping in this script for '$author'\n";
+		next;
+	}
+
+	# Fetch the image!
+	my $grav_url = "http://www.gravatar.com/avatar.php?gravatar_id=$gravatar&d=404&size=" . $size;
+	warn "Fetching image for '$author' ($grav_url)...\n";
+	my $rc = getstore( $grav_url, $author_image_file );
+
+	# Anything other than a 200 meant we didn't get the image
+	if ( $rc != 200 ) {
+		warn "PAUSE id '" . $author_to_pauseid{ $author } . "' for committer '$author' does not have a gravatar!\n";
+
+		if ( -e $author_image_file ) {
+			unlink( $author_image_file ) or die "Unable to remove '$author_image_file': $!\n";
+		}
+	}
+
+	# Give our internets some rest!
+	sleep 1;
+}
+
+# All done!
+close $gitlog or die "Unable to close git-log: $!\n";
--- extras/gource/make_gource.sh
+++ extras/gource/make_gource.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
-# This script generates a Gource vid via ffmpeg - please feel free to hack on it!
+# This script generates a Gource video via ffmpeg - please feel free to hack on it!
 # Note: You need a git repo!
 # You can get it at: http://github.com/bingos/poe
+
+# Processing the git repo as of March 16, 2010 results in a 1m8s long video clocking in at 41.1MB
 
 if [ ! $1 ]
 then
@@ -11,7 +13,11 @@ then
 fi
 cd $1
 
-gource poe/ -1280x720 -s 0.01 --auto-skip-seconds 0.1 --user-image-dir poe/extras/gource/avatars/ \
-  --highlight-all-users --multi-sampling --file-filter "/CVSROOT|/htmldocs|/wiki|/extras|/poexs|/queue|/docs" \
-  --stop-on-idle --output-ppm-stream - | \
-  ffmpeg -y -b 3000K -r 24 -f image2pipe -vcodec ppm -i - -vcodec mpeg4 gource_POE.mp4
+# Extra options we could use?
+# --file-filter "/CVSROOT|/htmldocs|/wiki|/extras|/poexs|/queue|/docs"
+
+gource poe/ -1280x720 -s 0.001 --highlight-all-users --multi-sampling \
+  --user-image-dir poe/extras/gource/avatars/ --user-scale 1.5 \
+  --disable-bloom --elasticity 0.0001 --max-file-lag 0.000001 --max-files 1000000 \
+  --date-format "%B %d, %Y" --disable-progress --stop-on-idle \
+  --output-ppm-stream - | ffmpeg -y -b 5000K -r 40 -f image2pipe -vcodec ppm -i - -vcodec mpeg4 gource_POE.mp4
